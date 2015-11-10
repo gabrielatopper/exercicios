e6ec26ff3d5e10226ccd74e9aae714bc90fa3104
Gabriela dos Santos de Oliveira <04029698042@facinepe.local>
2015-11-10 16:43:48 -0200

    multiplicacao

--- salario.py
+++ salario.py
@@ -1,4 +1,4 @@
-# -*- coding: latim1 -*-
+﻿# -*- coding: latim1 -*-
 horas_trab = int(input("Informe o numero de horas trabalhadas em mês: "))
 print (horas_trab)
 salario_hora = int(input("informe o valor do salario por hora: "))
@@ -12,4 +12,9 @@ print ("guerrerira selvagem")
 def soma_selvagem(num1,num2):
 	resultado = num1+num2
 	return resultado
-soma_selvagem (2+2)
+soma_selvagem (2,2)
+
+def selvagem (num1,num2):
+        resultado = num1*num2
+        return resultado
+selvagem (8,8)
