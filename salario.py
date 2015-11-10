0bc2d09ef5ca97dcbbf58fe7b770dce00520a32c
gabrielatopper <gabriela.topper2@gmail.com>
2015-11-10 15:42:09 -0200

    modificacao1

---
+++ salario.py
@@ -0,0 +1,9 @@
+# -*- coding: latim1 -*-
+horas_trab = int(input("Informe o numero de horas trabalhadas em mês: "))
+print (horas_trab)
+salario_hora = int(input("informe o valor do salario por hora: "))
+print(salario_hora)
+
+
+
+## hahahahaha
