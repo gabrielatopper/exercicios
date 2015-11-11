bbf5ec13c9f3a91bebd35cff7574e6b2156440f0
gabrielatopper <gabriela.topper2@gmail.com>
2015-11-11 18:39:29 -0200

    ordem

---
+++ ordem.py
@@ -0,0 +1,10 @@
+num1 = int(input("Informe o primeiro numero: "))
+print (num1)
+num2 = int (input("Informe o segundo numero: "))
+print (num2)
+if num1==num2:
+	print ("Numeros iguais")
+if num1 > num2:
+	print ("Primeiro maior")
+if num2 > num1:
+	print ("Segundo maior")
