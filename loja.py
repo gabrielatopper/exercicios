ba19d6a101f99b855c3d8d51698f997e1d985169
gabrielatopper <gabriela.topper2@gmail.com>
2015-11-24 14:44:02 -0200

    loja

--- loja.py
+++ loja.py
@@ -1,9 +1,10 @@
 total_estoque = 0
+media = 0
 valor_mercadorias = int (input("Informe o valor de cada mercadoria: "))
 
-for i in range (1, num_mercadorias)
+for i in range (1, num_mercadorias+1):
 	num_mercadorias = int (input("Informe o total de mercadorias:"))
-
+	print (num_mercadorias)
 total_estoque = total_estoque + valor_mercadorias
 media = total_estoque / num_mercadorias
 print(media)
