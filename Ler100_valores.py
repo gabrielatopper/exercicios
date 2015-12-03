3a567a4fca93fdd1fdd2822ff15a8f86f6348e49
gabrielatopper <gabriela.topper2@gmail.com>
2015-12-03 16:43:41 -0200

    valores

--- Ler100_valores.py
+++ Ler100_valores.py
@@ -1,7 +1,16 @@
-n = int(input("Informe um numero: "))
-
-for i in range (1, 101):
-	n = int(input("Informe um numero: "))
-	print (i)
-
-if 
+num = int(raw_input("Informe um numero: "))
+menor = num
+maior = num 
+print (menor)
+print (maior)
+for x in range (1, 9):
+	num = int (raw_input("informe um numero: "))
+	if ((num <= menor) and (num >=maior)):
+		menor = num
+		maior = num
+	elif ((num <= menor) and (num<=maior)):
+		menor = num
+	elif ((num >= maior) and (num >= menor)):
+		maior = num
+	print (maior)
+	print (menor)
