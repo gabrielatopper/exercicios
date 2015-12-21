1bde386a8af6779f38c6494fc29fe3b0ce398350
gabrielatopper <gabriela.topper2@gmail.com>
2015-12-21 16:25:15 -0200

    Delete sal.py

--- sal.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: latim1 -*-
-salario_fixo = int (input ("Informe o salario fixo: "))
-print (salario_fixo)
-valor_vendas = int(input("Informe o valor das vendas efetuadas pelo vendedor: "))
-print(valor_vendas)
